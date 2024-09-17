a577608d51ef95dd78773764ba11fbfb0175ea2b
Avyakta Wrat <avyaktawrat@microsoft.com>
2024-09-17 20:08:08 +0530

    reverting changes to afternoon

--- Startup_Code/detect_object.py
+++ Startup_Code/detect_object.py
@@ -29,26 +29,21 @@ def detect_color_shape(image, color, shape):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     # rgb -> bgr
     if color == "green":
-        # color_bgr = np.uint8([[[15,  164, 114]]])
-        color_bgr = np.uint8([[[33,  159, 87]]])
-        # color_bgr = np.uint8([[[51,  154, 60]]])
+        color_bgr = np.uint8([[[15,  164, 114]]])
     if color == "red":
         color_bgr = np.uint8([[[181,  41, 37]]])
     if color == "blue":
-        # color_bgr = np.uint8([[[0,  114, 171]]])
-        color_bgr = np.uint8([[[3,  48, 106]]])
+        color_bgr = np.uint8([[[0,  114, 171]]])
     if color == "orange":
         color_bgr = np.uint8([[[238,  181, 58]]])
 
     hsv_ = cv2.cvtColor(color_bgr, cv2.COLOR_BGR2HSV)
-    # print(hsv_)
-    lower = np.array([max(int(hsv_[0][0][0]) - 15, 0), 100, 100])
-    upper = np.array([min(int(hsv_[0][0][0]) + 15, 179), 255, 255])
+    lower = np.array([hsv_[0][0][0] - 10, 100, 100])
+    upper = np.array([hsv_[0][0][0] + 10, 255, 255])
 
     mask = cv2.inRange(hsv, lower, upper)
     cv2.imshow('mask', mask)
     cv2.waitKey(1)
-    cv2.destroyAllWindows
 
     blurred_mask = cv2.GaussianBlur(mask, (7, 7), 0)
     # cv2.imshow('blurred mask', blurred_mask)
@@ -64,20 +59,20 @@ def detect_color_shape(image, color, shape):
         if area < 800:
             continue
 
-        approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+        approx = cv2.approxPolyDP(contour, (0.01 if shape=="circle" else 0.1) * cv2.arcLength(contour, True), True)
         cx, cy = None, None
-        print("Number of contours detected: ", len(approx))
+        print(len(approx))
 
-        if len(approx) >= 4 and len(approx) <= 8:
+        if len(approx) == 4:
             shape_detected = "square"
             cx, cy = get_centroid(contour, image)
             # display(image, contour, shape_detected, approx)
-        elif len(approx) >= 9:
+        elif len(approx) > 13:
             shape_detected = "circle"
             cx, cy = get_centroid(contour, image)
             # display(image, contour, shape_detected, approx)
         
-        if cx is not None and cy is not None and (shape_detected == shape):
+        if cx is not None and cy is not None:
             delta = 20
             right = 320 + delta
             left = 320 - delta
@@ -95,10 +90,8 @@ def detect_color_shape(image, color, shape):
     return (False, "")
 
 if __name__ == "__main__":
-    # image = cv2.imread(".\images\output_green.jpg")
-    # ret = detect_color_shape(image, "green", "circle")
     while True:
         time.sleep(1)
         image = get_frame()
-        ret = detect_color_shape(image, "blue", "circle")
-        # print("Detected color and shape: ", ret)
+        ret = detect_color_shape(image, "orange", "circle")
+        print("Detected color and shape: ", ret)
